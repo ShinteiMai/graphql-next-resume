69b9baf6a2757a0ab0eac019b4a50a89b7f0cb9c
ShinteiMai <stevenhanselgo@gmail.com>
2020-11-17 13:13:16 +0700

    :wrench: Fixed the goddamn react-spring Trail issue

--- components/Common/Trail.tsx
+++ components/Common/Trail.tsx
@@ -1,35 +1,32 @@
 import React from "react";
 import { useTrail, animated } from "react-spring";
-import { TComponent } from "..";
 
-interface Props extends TComponent {
+type Props = {
   children: React.ReactNode;
   open: boolean;
   className?: string;
-}
+};
 
-const Trail = ({
-  open,
-  children,
-  className,
-  "data-testid": testId,
-  ...props
-}: Props) => {
+const Trail = ({ open, children, className, ...props }: Props) => {
   const items = React.Children.toArray(children);
   const trail = useTrail(items.length, {
     config: { mass: 5, tension: 2000, friction: 200 },
-    height: open ? "110px" : 0,
-    from: { x: 20, height: 0 },
+    opacity: open ? 1 : 0,
+    x: open ? 0 : 20,
+    height: open ? 110 : 0,
+    from: { opacity: 0, x: 20, height: 0 },
   });
   return (
-    <div data-testid={testId || "trail"} {...props}>
+    <div {...props}>
       <div>
-        {trail.map(({ x, height, ...rest }, index) => (
+        {trail.map(({ x, height, opacity, ...rest }, index) => (
           <animated.div
             key={index}
             style={{
               ...rest,
-              transform: x.interpolate((x) => `translate3d(0,${x}px,0)`),
+              opacity: opacity as any,
+              // @ts-ignore
+              transform: x.to((x) => `translate3d(0,${x}px,0)`),
             }}
           >
             <animated.div className={className}>{items[index]}</animated.div>
