3a4abf4a64320eb61ab32e0e458c797761ae49cb
ShinteiMai <stevenhanselgo@gmail.com>
2020-12-27 14:32:58 +0700

    :wrench: Fixed postcss setup

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,29 +1,26 @@
 import data from "../data/profile.json";
 import { technologies } from "../data/technologies.json";
 
-import Container from "../components/UI/Container";
-import Heading from "../components/UI/Heading";
-import Paragraph from "../components/UI/Paragraph";
 import TextLink from "../components/UI/TextLink";
 import Experience from "../components/Common/Experience";
 import Technology from "../components/Common/Technology";
 
 const IndexPage = () => {
   return (
-    <Container className="w-full lg:w-8/12">
-      <Container className="mt-0 md:mt-4 lg:mt-6">
-        <Heading className="text-xl font-medium">Biography</Heading>
-        <Paragraph className="mt-3">
+    <div className="w-full lg:w-8/12">
+      <div className="mt-0 md:mt-4 lg:mt-6">
+        <h1 className="text-xl font-medium">Biography</h1>
+        <p className="mt-3">
           Hi! I’m Steven Hansel. A software engineer based in Jakarta,
           Indonesia.
-        </Paragraph>
-        <Paragraph className="mt-3">
+        </p>
+        <p className="mt-3">
           I enjoy building software such as web & mobile applications with the
           most modern & latest technology available. My purpose is to create
           robust software following software engineering principles & best
           practices.
-        </Paragraph>
-        <Paragraph className="mt-3">
+        </p>
+        <p className="mt-3">
           I’m currently studying Computer Engineering in{" "}
           <TextLink href={data.links.binus} newTab className="text-primary">
             BINUS University
@@ -34,15 +31,15 @@ const IndexPage = () => {
             Kotakode
           </TextLink>
           .
-        </Paragraph>
-        <Container className="mt-8">
-          <Heading className="text-xl font-medium mb-5">Experience</Heading>
+        </p>
+        <div className="mt-8">
+          <h1 className="text-xl font-medium mb-5">Experience</h1>
           <Experience />
-        </Container>
+        </div>
 
-        <Container className="mt-8">
-          <Heading className="text-xl font-medium">Stacks</Heading>
-          <Container className="grid grid-cols-4 lg:grid-cols-8 mt-4 gap-y-6">
+        <div className="mt-8">
+          <h1 className="text-xl font-medium">Stacks</h1>
+          <div className="grid grid-cols-4 lg:grid-cols-8 mt-4 gap-y-6">
             {technologies.map(({ id, thumbnail, title, link }) => (
               <Technology
                 src={thumbnail}
@@ -54,10 +51,10 @@ const IndexPage = () => {
                 height={50}
               />
             ))}
-          </Container>
-        </Container>
-      </Container>
-    </Container>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,7 +1,6 @@
 const purgecss = [
   "@fullhuman/postcss-purgecss",
   {
-    // content: ["./components/**/*.js", "./pages/**/*.js"],
     content: [
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
