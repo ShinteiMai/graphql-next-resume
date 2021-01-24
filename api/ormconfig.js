26d5c2208c956fde3f648af32ac233da8dacd338
ShinteiMai <stevenhanselgo@gmail.com>
2021-01-24 11:17:00 +0700

    :wrench: Fixed seeding process

---
+++ api/ormconfig.js
@@ -0,0 +1,15 @@
+/** ormconfig.js is only used for typeorm-seeding, for real-connection useCase, check setupTypeORMConnection.ts  */
+module.exports = {
+  name: "default",
+  type: "postgres",
+  host: process.env.NODE_ENV === "production" ? "database" : "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "password",
+  database: "stevenhansel",
+  synchronize: true,
+  logging: false,
+  entities: ["src/database/entity/**/*.*"],
+  seeds: ["src/database/seeds/**/*{.ts,.js}"],
+  factories: ["src/database/factories/**/*{.ts,.js}"],
+};
--- api/src/database/seeds/create-users.seed.ts
+++ api/src/database/seeds/create-users.seed.ts
@@ -1,8 +1,15 @@
-import { User } from "@db/entity";
+import { User } from "../entity";
 import { Factory, Seeder } from "typeorm-seeding";
 
 export default class CreateUsers implements Seeder {
-  public async run(factory: Factory): Promise<any> {
+  public async run(factory: Factory): Promise<void> {
+    await factory(User)().create({
+      email: "test@test.com",
+      password: "asdasd",
+      firstName: "Steven",
+      lastName: "Hansel",
+      confirmed: true,
+    });
     await factory(User)().createMany(10);
   }
 }
